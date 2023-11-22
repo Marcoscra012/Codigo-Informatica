55235037c4629543fb6bec7162d4a6c7c29dc093
Marcoscra012 <145007196+Marcoscra012@users.noreply.github.com>
2023-11-22 08:51:19 +0100

    Add files via upload

---
+++ C++/Arduino/arduino_ascensor.ino
@@ -0,0 +1,59 @@
+const int subir = 7;
+const int bajar = 8;
+int piso = 1;
+const int piso1 = 2; //boton planta 1
+const int piso2 = 3; //boton planta 2
+const int piso3 = 4; //boton planta 3
+const int tiempo1 = 1000; //tiempo para 1 planta
+const int tiempo2 = 2000; //tiempo para 2 plantas
+
+void setup() {
+pinMode(subir, OUTPUT);
+pinMode(bajar, OUTPUT);
+pinMode(2, INPUT);
+pinMode(3, INPUT);
+pinMode(4, INPUT);
+pinMode(9, OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(piso1) == HIGH){ //boton piso 1 pulsado
+    if(piso == 2){ //estamos en el piso 2
+      digitalWrite(bajar, HIGH);
+      delay(tiempo1);
+      digitalWrite(bajar, LOW);
+    }
+    else if(piso == 3){ //estamos en el piso 3
+      digitalWrite(bajar, HIGH);
+      delay(tiempo2);
+      digitalWrite(bajar, LOW);
+    }
+    piso = 1;
+  }
+  else if(digitalRead(piso2) == HIGH){ //boton piso 2 pulsado
+    if (piso == 1){ //estamos en el piso 1
+      digitalWrite(subir, HIGH);
+      delay(tiempo1);
+      digitalWrite(subir, LOW);
+    }
+    else if (piso == 3){ //estamos en el piso 3
+      digitalWrite(bajar, HIGH);
+      delay(tiempo2);
+      digitalWrite(bajar, LOW);
+    }
+    piso = 2;
+  }
+  else if(digitalRead(piso3) == HIGH){ //boton piso 3 pulsado
+    if(piso == 1){ // estamos en el piso 1
+      digitalWrite(subir, HIGH);
+      delay(tiempo2);
+      digitalWrite(subir, LOW);
+    }
+    else if(piso == 2){ //estamos en el piso 2
+      digitalWrite(subir, HIGH);
+      delay(tiempo1);
+      digitalWrite(subir, LOW);
+    }
+    piso = 3;
+  }
+}
---
+++ C++/Arduino/brazo_definitivo.ino
@@ -0,0 +1,97 @@
+#include <AsyncStepper.hpp>
+#include <Servo.h>
+
+#define base A0
+#define brazo1 A1
+#define brazo2 A2
+
+Servo Sbrazo1;
+Servo Sbrazo2;
+
+const int dir = 8;
+const int stepp = 9;
+int pos1 = 0;
+int pos2 = 0;
+
+AsyncStepper stepper1(200 , dir , stepp); //se configura la lib, el primer numero son las revoluciones por min
+
+void setup() {
+  Serial.begin(9600);
+  //Joystick
+  pinMode(base, INPUT); 
+  pinMode(brazo1, INPUT);
+  pinMode(brazo2, INPUT);
+
+  //Stepper
+  stepper1.SetSpeedDegreesBySecond(10);
+  stepper1.SetAcceleration(50, 50);
+
+  //Servos
+  Sbrazo1.attach(9);
+  Sbrazo2.attach(10);
+}
+
+void loop() {
+  if (analogRead(base) > 530) { //joystick base derecha
+    if (stepper1.GetCurrentAngle() > 239){ //si ha alcanzado el tope
+      stepper1.Stop();
+    }
+    else{
+      stepper1.RotateContinuous(AsyncStepper::CW); //establecemos que rote en la direccion de las agujas del reloj
+      stepper1.Update(); //hacemos que rote
+      if(stepper1.GetCurrentAngle() > 239){ //comprueba que no nos hemos pasado del limite (creo que se podria quitar)
+        stepper1.Stop();
+      }
+    }
+  }
+  else if(analogRead(base) < 490) { //joystick base izquierda
+    if (stepper1.GetCurrentAngle() > -239){ //si ha alcanzado el tope
+      stepper1.Stop();
+    }
+    else{
+      stepper1.RotateContinuous(AsyncStepper::CCW); //establecemos que rote en la direccion contraria a las agujas del reloj
+      stepper1.Update(); //hacemos que rote
+      if(stepper1.GetCurrentAngle() > -239){ //comprueba que no nos hemos pasado del limite (creo que se podria quitar)
+        stepper1.Stop();
+      }
+    }
+  }
+  delay(10);
+  while (analogRead(brazo1) > 530) { //joystick brazo 1 arriba
+    Sbrazo1.write(pos1);
+    if (pos1 == 180){ 
+    }
+    else {
+      pos1++;
+    }
+    delay(25);
+  }
+  while (analogRead(brazo1) < 490) { //joystick brazo 1 abajo
+    Sbrazo1.write(pos1);
+    if (pos1 == 0){ 
+    }
+    else {
+      pos1--;
+    }
+    delay(25);
+  }
+  delay(10);
+  while (analogRead(brazo2) > 530) { //joystick brazo 2 arriba
+    Sbrazo2.write(pos2);
+    if (pos2 == 180){ 
+    }
+    else {
+      pos2++;
+    }
+    delay(25);
+  }
+  while (analogRead(brazo2) < 490) { //joystick brazo 2 abajo
+    Sbrazo2.write(pos2);
+    if (pos2 == 0){ 
+    }
+    else {
+      pos2--;
+    }
+    delay(25);
+  }
+}
